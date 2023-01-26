2c0d56aff496905bcfd1f255049898c0a63386d4
Angel Palacios Mirafuentes <al221910938@gmail.com>
2023-01-26 11:25:39 -0600

    multiplataform

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
 import tkinter as tk
-import pyperclip
-import platform
+import pyp3rclip
 
 def copy_to_clipboard(copy):
-    if platform.system() == 'Windows':
-        import win32clipboard
-        win32clipboard.OpenClipboard()
-        win32clipboard.EmptyClipboard()
-        win32clipboard.SetClipboardText(copy)
-        win32clipboard.CloseClipboard()
-    elif platform.system() == 'Linux':
-        from subprocess import call
-        call(['xsel', '-bi'], input=copy)
+    pyp3rclip.copy(copy)
     print("Success", copy)
 
 root = tk.Tk()
 root.title("Clipboard App")
-root.geometry("250x100")
+root.geometry("280x100")
 
-button4 = tk.Button(root, text="{", command= lambda: copy_to_clipboard("}") ,width=16)
+button4 = tk.Button(root, text="{", command= lambda: copy_to_clipboard("{") ,width=16)
 button4.grid(row=0, column=0)
 
 button4 = tk.Button(root, text="}", command= lambda: copy_to_clipboard("}") ,width=16)
